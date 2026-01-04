ff8205497d429632ac8d078527414cb700af41a9
Ashraf <ventureofficial1606@gmail.com>
2026-01-04 18:08:11 +0530

    feat: add Process service with CRUD operations, file upload support, and dynamic select options.

--- src/services/Process.js
+++ src/services/Process.js
@@ -7,8 +7,8 @@ import { setAllProcesses } from "../redux/slices/department";
 import { setSelectOptionsArray } from "../redux/slices/auth";
 
 function Process() {
-  // const URL = "https://adl-server.onrender.com/api/v1/process";
-  const URL = "http://localhost:3008/api/v1/process";
+  const URL = "https://adl-server.onrender.com/api/v1/process";
+  // const URL = "http://localhost:3008/api/v1/process";
 
   const allProcesses = useSelector((state) => state.department.allProcesses);
 
